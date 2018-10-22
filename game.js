e44696121b540571d4744327523e794e2903c96f
praktikum <praktikum@connedata.de>
2018-10-22 12:18:57 +0200

    Init

--- game.js
+++ game.js
@@ -1,38 +1,50 @@
 var game = new ex.Engine({
   width: 800,
   height: 600
+
 });
 
 game.setAntialiasing(false);
 game.backgroundColor = ex.Color.Black;
 
-var loader = new ex.Loader();
-var audio = new Audio('/home/praktikum/git/breakout/sound/bounce.wav');
-var audio1 = new Audio('/home/praktikum/git/breakout/sound/bounce.wav');
-var audio2 = new Audio('/home/praktikum/git/breakout/sound/bounce.wav');
-var intro = new Audio('/home/praktikum/git/breakout/sound/Intro.wav');
-var verloren = new Audio('/home/praktikum/git/breakout/sound/verloren.wav')
+var audio = new Audio('/home/praktikum/git/breakout/sound/plop.wav');
+var loader = new ex.Loader(audio);
+
+var intro = new Audio('/home/praktikum/git/breakout/sound/happy.wav');
+var verloren = new Audio('/home/praktikum/git/breakout/sound/gameover.wav')
+var gewonnen = new Audio('/home/praktikum/git/breakout/sound/win.wav')
+var powerup = new Audio('home/praktikum/git/breakout/sound/powerup.wav')
+var badpowerup = new Audio('home/praktikum/git/breakout/sound/wrongitem.wav')
+
 
 game.start(loader);
 
-
 // Position und Größe
-var paddle = new ex.Actor(50, game.drawHeight - 40, 150, 20);
+var paddle = new ex.Actor(50, game.drawHeight - 40, 50, 20);
+var paddle1 = new ex.Actor(50, game.drawHeight - 40, 50, 20);
+var paddle2 = new ex.Actor(50, game.drawHeight - 40, 50, 20);
 
 // Farbe
 paddle.color = ex.Color.Chartreuse;
-
+paddle1.color = ex.Color.White;
+paddle2.color = ex.Color.Blue;
 // Kollision
 paddle.collisionType = ex.CollisionType.Fixed;
+paddle2.collisionType = ex.CollisionType.Fixed;
+paddle1.collisionType = ex.CollisionType.Fixed;
 
 // Objekt hinzufügen
 game.add(paddle);
+game.add(paddle1);
+game.add(paddle2);
 
 intro.play();
 intro.volume = 0.2;
 // Maus bewegungen erkennen
 game.input.pointers.primary.on('move', function(evt) {
   paddle.pos.x = evt.worldPos.x;
+  paddle1.pos.x = (evt.worldPos.x)-50;
+  paddle2.pos.x = evt.worldPos.x + 50;
 });
 
 // Ball entstehen lassen
@@ -61,13 +73,13 @@ ball.on('postupdate', function() {
   // Rechts
   if (this.pos.x + (this.getWidth() / 2) > game.drawWidth) {
     this.vel.x *= -1;
-    audio1.play();
+    audio.play();
   }
 
   // Oben
   if (this.pos.y < (this.getHeight() / 2)) {
     this.vel.y *= -1;
-    audio2.play();
+    audio.play();
   }
 });
 
@@ -84,8 +96,9 @@ ball.draw = function(ctx, delta) {
 game.add(ball);
 
 ball.on('exitviewport', function() {
-  alert('You lose!');
-  document.location.reload();
+  verloren.play();
+
+
 });
 
 // Lücken zwischen und neben den Blöcken
@@ -103,7 +116,7 @@ var b = 0
 
 // Blöcke und Items erzeugen
 var brickWidth = game.drawWidth / columns - padding - padding / columns; // px
-var itemWidth =  (game.drawWidth / columns - padding - padding / columns)/3.75 ; // px
+var itemWidth = (game.drawWidth / columns - padding - padding / columns) / 3.75; // px
 var brickHeight = 30;
 var itemHeight = 20;
 var bricks = [];
@@ -112,28 +125,35 @@ for (var j = 0; j < rows; j++) {
   a++
   for (var i = 0; i < columns; i++) {
     b++
-    if (i%2==1) {
+    if (i % 2 == 1) {
       var speedy = ex.Actor.extend({
         typ: "Speedy",
-        getTyp: function () {
+        getTyp: function() {
           return (this.typ);
         },
-        getSpeed: function () {
+        getSpeed: function() {
           return (1.2);
+
         }
+
       });
-      var actor = new speedy(xoffset + i * ((itemWidth + padding )*2.75) + padding, yoffset + j * ((itemHeight + padding)*1.2) + padding, itemWidth, itemHeight, ex.Color.Green)
+
+      var actor = new speedy(xoffset + i * ((itemWidth + padding) * 2.75) + padding, yoffset + j * ((itemHeight + padding) * 1.2) + padding, itemWidth, itemHeight, ex.Color.Green);
+
     } else {
+
       var slowy = ex.Actor.extend({
         typ: "Slowy",
-        getTyp: function () {
+        getTyp: function() {
           return (this.typ);
         },
-        getSpeed: function () {
+        getSpeed: function() {
           return (0.9);
+
         }
       });
-      var actor = new slowy(xoffset + i * ((itemWidth + padding )*2.75) + padding, yoffset + j * ((itemHeight + padding)*1.2) + padding, itemWidth, itemHeight, ex.Color.Red)
+
+      var actor = new slowy(xoffset + i * ((itemWidth + padding) * 2.75) + padding, yoffset + j * ((itemHeight + padding) * 1.2) + padding, itemWidth, itemHeight, ex.Color.Red)
     }
 
     // Item Array erzeugen
@@ -173,6 +193,7 @@ ball.on('precollision', function(ev) {
 
     collisionItem.collisionType = ex.CollisionType.Passive;
     collisionItem.on('precollision', function(ev) {
+
       if (ev.other.id == 0) {
 
         // Paddle getroffen, geht bestimmt noch besser,
@@ -181,16 +202,17 @@ ball.on('precollision', function(ev) {
         // Speedup aus dem Item holen
         speedup = collisionItem.getSpeed();
 
+
         ball.vel.x = ball.vel.x * speedup;
         ball.vel.y = ball.vel.y * speedup;
 
-         // zerstöre das Item
+        // zerstöre das Item
         collisionItem.kill();
       }
     });
 
     // Item fallen lassen
-    collisionItem.vel.setTo(0,200);
+    collisionItem.vel.setTo(0, 200);
 
     // Punktzahl erhöhen
     score++;
@@ -201,8 +223,7 @@ ball.on('precollision', function(ev) {
   }
 
   if (score == bricks.length) {
-    alert("Du hast Gewonnen!");
-    document.location.reload();
+    gewonnen.play();
   }
 
   // reverse course after any collision
